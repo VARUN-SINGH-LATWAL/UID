e4c5f7fdcf3ea74e79cbf7f60175225f1067158c
VARUN-SINGH-LATWAL <varunsingh0456@gmail.com>
2025-10-28 16:29:09 +0530

    add new test api

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,4 +6,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express!");
 });
 
+app.get("/Home", (req: Request, res: Response) => {
+  res.send("Home, Page ");
+});
+
+
+
 app.listen(3000, () => console.log("Server running on port 3000"));
