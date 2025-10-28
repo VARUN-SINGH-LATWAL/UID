713734b49eff83c442d298978e754b8ce143c83b
VARUN-SINGH-LATWAL <varunsingh0456@gmail.com>
2025-10-28 16:30:27 +0530

    remove test api

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express!");
 });
 
-app.get("/Home", (req: Request, res: Response) => {
-  res.send("Home, Page ");
-});
-
 
 
 app.listen(3000, () => console.log("Server running on port 3000"));
