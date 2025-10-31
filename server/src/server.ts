03dce1edeb4a271a8c8812f3ab781975cd017c74
VARUN-SINGH-LATWAL <varunsingh0456@gmail.com>
2025-10-31 17:12:03 +0530

    ADD PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,10 +6,12 @@ const app = express();
 
 checkConnection()
 
+const PORT : Number = 3000 
+
 // start api 
 
 app.use('/api',Table)
 
 
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log("Server running on port 3000"));
